7d8a4ff73a0f7a425f9583aa3c9c62d14999606e
N1k312 <obemeaylol@gmail.com>
2026-04-24 12:46:31 +0500

    refactor(splash): remove manual navigation timer (redirect handles it)

--- lib/features/auth/screens/splash_screen.dart
+++ lib/features/auth/screens/splash_screen.dart
@@ -2,13 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_spacing.dart';
 import '../../../core/constants/app_text_styles.dart';
 import '../../../core/constants/strings.dart';
-import '../../../core/router/route_paths.dart';
 import '../../../core/widgets/app_logo.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
@@ -21,7 +19,6 @@ class SplashScreen extends ConsumerStatefulWidget {
 class _SplashScreenState extends ConsumerState<SplashScreen> {
   bool _showProgress = false;
   Timer? _progressTimer;
-  Timer? _navTimer;
 
   @override
   void initState() {
@@ -29,15 +26,11 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
     _progressTimer = Timer(const Duration(milliseconds: 800), () {
       if (mounted) setState(() => _showProgress = true);
     });
-    _navTimer = Timer(const Duration(seconds: 2), () {
-      if (mounted) context.go(AppRoutes.login);
-    });
   }
 
   @override
   void dispose() {
     _progressTimer?.cancel();
-    _navTimer?.cancel();
     super.dispose();
   }
 
